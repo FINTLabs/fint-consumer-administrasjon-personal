ef3a23751800b8cce164138454c00dd5c3c673fa
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2024-04-15 10:47:01 +0200

    Ct 1079 new fint relations (#51)
    * upgrade relations version & implement cache changes
    
    * update fint-cache dependency
    
    ---------
    
    Co-authored-by: Hknots <hknots99@gmail.com>

--- src/main/java/no/fint/consumer/models/fastlonn/FastlonnCacheService.java
+++ src/main/java/no/fint/consumer/models/fastlonn/FastlonnCacheService.java
@@ -117,7 +117,10 @@ public class FastlonnCacheService extends CacheService<FastlonnResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (PersonalActions.valueOf(event.getAction()) == PersonalActions.UPDATE_FASTLONN) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<FastlonnResource>> cacheObjects = data
--- src/main/java/no/fint/consumer/models/fasttillegg/FasttilleggCacheService.java
+++ src/main/java/no/fint/consumer/models/fasttillegg/FasttilleggCacheService.java
@@ -117,7 +117,10 @@ public class FasttilleggCacheService extends CacheService<FasttilleggResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (PersonalActions.valueOf(event.getAction()) == PersonalActions.UPDATE_FASTTILLEGG) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<FasttilleggResource>> cacheObjects = data
