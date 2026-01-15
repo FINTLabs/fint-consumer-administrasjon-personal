0a631ba5616bdb3ef53846ef602398f524b50c33
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-15 15:27:49 +0100

    Version 4.0.0-rc-6.

---
+++ src/main/java/no/novari/fint/consumer/utils/RestEndpoints.java
@@ -0,0 +1,16 @@
+package no.novari.fint.consumer.utils;
+
+public enum RestEndpoints {
+    ;
+
+    public static final String ADMIN = "/admin";
+	public static final String ARBEIDSFORHOLD = "/arbeidsforhold";
+	public static final String FASTLONN = "/fastlonn";
+	public static final String FASTTILLEGG = "/fasttillegg";
+	public static final String FRAVAR = "/fravar";
+	public static final String KONTAKTPERSON = "/kontaktperson";
+	public static final String PERSON = "/person";
+	public static final String PERSONALRESSURS = "/personalressurs";
+	public static final String VARIABELLONN = "/variabellonn";
+
+}
